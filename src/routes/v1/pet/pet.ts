c413492260c1dabfd515ce7fb1faf425aecf1076
shin donggyu <donggyu9410@gmail.com>
2020-11-07 15:36:43 +0900

    반려 동물을 등록하는 api 개발발

--- src/routes/v1/pet/pet.ts
+++ src/routes/v1/pet/pet.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { getPetList } from "../../../controllers/pets/pet";
+import { postNewPet } from "../../../controllers/pets/PostPet";
 
 const router = express.Router();
 
+router.post("", postNewPet);
 router.get("/list", getPetList);
 
 export default router;
