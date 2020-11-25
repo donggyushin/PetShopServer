d9b4bc2bb787b4a79dad8d734a925b7f0a29fb83
shin donggyu <donggyu9410@gmail.com>
2020-11-26 02:25:55 +0900

    admin sdk 추가

--- src/push/index.ts
+++ src/push/index.ts
@@ -1,3 +1,39 @@
+import * as admin from "firebase-admin";
+
+const serviceAccount = require("../../keys/petmily-dab67-firebase-adminsdk-rorl7-151d8b8daf.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const myPhoneFcmToken =
+  "cniM_irIFUqGo_i1nB3ksa:APA91bEyjo0zSVIkDOFmPMXvHNGuI2nEnZRS4ukR0ldnoUANN2vD3jJvNOFQvRG_TCBp2yKV3QnppmO6YzAcxDrMWZPaKpxey7QipDMSKlOxXcx-flnuBpTj1iAw8ieBjivLspZtJcRO";
+
+const sendPushNotification = (
+  message: string,
+  token: string,
+  data: { [key: string]: string } | undefined,
+  title?: string
+) => {
+  const notification = {
+    data,
+    token,
+    notification: {
+      title: "test",
+      body: "asd",
+    },
+  };
+  admin
+    .messaging()
+    .send(notification)
+    .then((response) => {
+      console.log("Successfully sent message", response);
+    })
+    .catch((err) => {
+      console.log("Error sending message: ", err);
+    });
+};
+
 // ㅎㅏ루에 한번씩 동작하는 함수
 const testFunction = () => {
   setTimeout(() => {
@@ -5,3 +41,7 @@ const testFunction = () => {
     testFunction();
   }, 1000 * 60 * 60 * 24);
 };
+
+sendPushNotification("test", myPhoneFcmToken, { test: "test" });
+
+export default admin;
